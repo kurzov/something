3f3e12cd02310d04d4182d164b1dcd669dcb6f0e
Yuri <kurzov.y@gmail.com>
2021-06-20 19:07:56 +0300

    mobile v + animation 2.5h

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 jQuery(document).ready(function($){
+	$('html').toggleClass('no-js js');
 
-
+	// animate
+	$(window).on('scroll', function(){
+		scrollTop = $(window).scrollTop();
+		
+		$(".anim").each(function(key){
+			if (scrollTop > ($(this).offset().top + $(this).height() )) {
+				$(this).removeClass('animated');
+			} else if (scrollTop > ($(this).offset().top - ($(window).height() - ($(window).height() / 10)) )) {
+				$(this).addClass('animated');
+			}else{
+				$(this).removeClass('animated');
+			}
+		});
+	});
 	
+	$(window).trigger('scroll');
 });
